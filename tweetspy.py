3b980b3a6626fbfbf2bced52b73158b2b54d4965
Etum <catsarebestthing@outlook.com>
2020-02-12 16:38:59 +0100

    basic info

---
+++ tweetspy.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+#TweetSpy, inspired by the tool CeWL.
+#TweetSpy is intended to create a wordlist from words that the twitter account has posted.
+#This tool is not intended for nefarious usage.
+
